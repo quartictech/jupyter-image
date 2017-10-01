0a99a48829700b448f4f87203b23d9a78d290c46
Oliver Charlesworth <oliver@quartic.io>
2017-10-01 13:13:47 +0100

    Switch over to new container registry

--- build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-set -eu
-
-# TODO: inject ${VERSION} into setup.py somehow
-VERSION=${CIRCLE_BUILD_NUM-unknown}
-QUARTIC_DOCKER_REPOSITORY=${QUARTIC_DOCKER_REPOSITORY-quartic}
-
-docker build -t ${QUARTIC_DOCKER_REPOSITORY}/jupyter:${VERSION} .
-docker push ${QUARTIC_DOCKER_REPOSITORY}/jupyter:${VERSION}
